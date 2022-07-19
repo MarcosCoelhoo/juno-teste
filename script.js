31f3fa1cffd5269e7d93911df1ba3e3517a942e9
Marcos Coelho <mparav124@gmail.com>
2022-07-19 16:15:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const menu = document.querySelector(".menu"),
+  btnAbrir = document.querySelector(".abrir"),
+  btnFechar = document.querySelector(".fechar");
+
+function abrirMenu() {
+  menu.classList.toggle("ativo");
+}
+
+function fecharMenu() {
+  menu.classList.toggle("ativo");
+}
+
+btnAbrir.addEventListener("click", abrirMenu);
+btnFechar.addEventListener("click", fecharMenu);
+
+const btnScroll = document.querySelector(".btn-scroll");
+
+function scrollSuave(event) {
+  const section = document.querySelector("#topo");
+  section.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+}
+
+btnScroll.addEventListener("click", scrollSuave);
